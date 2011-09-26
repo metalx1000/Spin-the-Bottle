5f07742e18f922d564d0de891b4a379ce61cae83
Kevin Short <kev.l.short@gmail.com>
2011-09-26 10:21:12 -0400

    Added an expandable tab that displays the sound mute button and the option to change the background.

--- spinbottle.py
+++ spinbottle.py
@@ -10,6 +10,8 @@
 import pygame, sys, random
 from pygame import *
 
+# Calling mixer.pre_init() fixes the lag in playing sound that sometimes occurs.
+pygame.mixer.pre_init(44100, -16, 2, 512)
 pygame.mixer.init()
 
 w = 800
@@ -18,7 +20,7 @@ h = 400
 screen = pygame.display.set_mode((w,h))
 
 #Load Background Image
-bg = pygame.image.load("bg.png")
+bg = pygame.image.load("stone-bg.png")
 bg = pygame.transform.scale(bg,(w,h))
 
 class Bottle:
@@ -34,7 +36,7 @@ class Bottle:
         self.speedy = 0
         self.tork = 0
         self.speedt = 0
-        # Create a rect from the loaded image. --Kevin
+        # Create a rect from the loaded image.
         self.rect = self.image.get_rect()
         #Load Glass Sound
         self.sound1 = pygame.mixer.Sound("glass.wav") 
@@ -51,7 +53,7 @@ class Bottle:
                 self.speedy = 10
       
     def update(self):
-        # Here we're going to prevent the bottle from getting stuck on the edges of the screen. --Kevin
+        # Here we're going to prevent the bottle from getting stuck on the edges of the screen. 
         if self.x < 0:
             self.speedx = -self.speedx
             self.x = 1
@@ -69,7 +71,7 @@ class Bottle:
         self.x+=self.speedx
         self.y+=self.speedy
         
-        # Set the rect's position to the object's x and y positon. --Kevin
+        # Set the rect's position to the object's x and y positon. 
         self.rect.left, self.rect.top = self.x, self.y
 
         if self.speedx > 0:
@@ -82,7 +84,7 @@ class Bottle:
         elif self.speedy < 0:
             self.speedy+=.025
             
-        # These lines stop the bottle from sliding by setting the speed to zero if the bottle's speed is almost zero. --Kevin
+        # These lines stop the bottle from sliding by setting the speed to zero if the bottle's speed is almost zero.
         if (self.speedx > 0 and self.speedx < 0.5) or (self.speedx < 0 and self.speedx > -0.5):
             self.speedx = 0
         if (self.speedy > 0 and self.speedy < 0.5) or (self.speedy < 0 and self.speedy > -0.5):
@@ -98,14 +100,18 @@ class Bottle:
         print self.tork 
         self.image2 = pygame.transform.rotate(self.image,self.tork)
         
-        # Create a new rect from the rotated image. --Kevin
+        # Create a new rect from the rotated image.
         self.rect2 = self.image2.get_rect()
         
         # Set the center of the rotated image's rect to be in the same position as the center of the un-rotated image's rect. 
-        # This fixes the strange rotation. --Kevin
+        # This fixes the strange rotation. 
         self.rect2.center = self.rect.center
+        
+        # Make sure we can still see the bottle even if the options tab is open.
+        if tab.expanded and self.x < tab.x:
+            self.x += 10
 
-        # Now draw the image, using the rotated image's rect as the position. --Kevin
+        # Now draw the image, using the rotated image's rect as the position.
         screen.blit(self.image2, self.rect2)
         
 class Music:
@@ -118,7 +124,7 @@ class Music:
         self.playing = True
         self.music = pygame.mixer.Sound(music)
         self.music.play(loops=-1)
-        self.x = 0
+        self.x = 0 - self.width
         self.y = h - self.height
         
     def click(self, x, y):
@@ -134,10 +140,85 @@ class Music:
                     self.playing = True
                     pygame.mixer.unpause()
                 
+    def update(self, x):
+        self.x = x - 300
+        screen.blit(self.image, (self.x, self.y))
+        
+class Background:
+    """A class to change the background image."""
+    def __init__(self, image, x, y):
+        self.bg = pygame.image.load(image)
+        self.image = pygame.transform.scale(self.bg, (100, 50))
+        self.width = self.image.get_width()
+        self.height = self.image.get_height()
+        self.pos = [x, y]
+        self.x = x
+        self.y = y
+        
+    def click(self, x, y):
+        if x > self.x and x < self.x + self.width:
+            if y > self.y and y < self.y + self.height:
+                global bg
+                bg = pygame.transform.scale(self.bg, (w, h))
+                
+    def update(self, x):
+        self.x = x + (self.pos[0] - 300)
+        screen.blit(self.image, (self.x, self.y))
+                
+class OptionTab:
+    """A small tab that expands to show extra options."""
+    
+    def __init__(self):
+        self.icon = pygame.image.load("tab.png")
+        self.image = self.icon
+        self.width = self.image.get_width()
+        self.height = self.image.get_height()
+        self.x = 0
+        self.y  = h - self.height
+        self.expanded = False
+        
+        # Options.
+        self.music = Music("music.ogg")
+        self.red_bg = Background("redwood-bg.png", 5, 5)
+        self.stone_bg = Background("stone-bg.png", 110, 5)
+        
+        self.options = {"music":self.music, "red bg":self.red_bg, "stone bg":self.stone_bg}
+        
+    def click(self, x, y):
+        if x > self.x and x < self.x + self.width:
+            if y > self.y and y < self.y + self.height:
+                if not self.expanded:
+                    self.expanded = True
+                else:
+                    self.expanded = False
+                   
+        if self.expanded:
+            for thing in self.options:
+                self.options[thing].click(x, y)
+                    
+    def update(self):
+        if self.expanded:
+            self.image = pygame.transform.flip(self.icon, 1, 0)
+            
+            if self.x < 300:
+                self.x += 10
+                
+        else:
+            self.image = self.icon
+            
+            if self.x > 0:
+                self.x -= 10
+        
+        pygame.draw.rect(screen, (86, 152, 119), (0, 0, self.x, h))
+        screen.blit(self.music.image, (self.music.x, self.music.y))
+        for thing in self.options.keys():
+            self.options[thing].update(self.x)
+        screen.blit(self.image, (self.x, self.y))
 
 clock = pygame.time.Clock()
 bottle = Bottle()
-music = Music("music.ogg")
+#music = Music("music.ogg")
+tab = OptionTab()
 
 while 1:
     x,y = pygame.mouse.get_pos()
@@ -145,14 +226,18 @@ while 1:
     screen.blit(bg,(0,0))
     clock.tick(60)
     bottle.update()
-    screen.blit(music.image, (music.x, music.y))
+    tab.update()
+#    screen.blit(music.image, (music.x, music.y))
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
         elif event.type == MOUSEBUTTONDOWN:
-            bottle.click(x, y)
-            music.click(x, y)
+            tab.click(x, y)
+            # Make sure that the options tab isn't open before checking to see if the user clicked the bottle.
+            if not tab.expanded:
+                bottle.click(x, y)
+#            music.click(x, y)
 
     pygame.display.update()    
 
